8a735fdd67cb38036ce92023f5bb45967b7f1178
Toygoon <toygoon@gmail.com>
2022-01-19 00:17:43 +0900

    Block changed array to list

--- src/flash/FlashStorage.java
+++ src/flash/FlashStorage.java
@@ -1,16 +1,19 @@
 package flash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlashStorage {
-    public Block[] block;
+    public List<Block> block;
 
     public static final int BLOCK_COUNT = 10;
     public static final int MAX_ERASURE_LIMIT = 100;
 
     public FlashStorage() {
-        this.block = new Block[BLOCK_COUNT];
+        this.block = new ArrayList<>();
 
-        for (int i = 0; i < this.block.length; i++) {
-            block[i] = new Block();
+        for (int i = 0; i < BLOCK_COUNT; i++) {
+            this.block.add(new Block());
         }
     }
 
@@ -19,6 +22,11 @@ public class FlashStorage {
         System.out.println("[STORAGE] Total blocks : " + BLOCK_COUNT);
 
         for (int i = 0; i < BLOCK_COUNT; i++)
-            System.out.println("[STORAGE] Block " + i + " erase count : " + block[i].getCount());
+            System.out.println("[STORAGE] Block " + i + " erase count : " + block.get(i).getEraseCount() + ", data : " + block.get(i).getData());
+    }
+
+    public void printSimple() {
+        for (int i = 0; i < BLOCK_COUNT; i++)
+            System.out.print("[" + i + "]:" + block.get(i).getEraseCount() + "  ");
     }
 }
