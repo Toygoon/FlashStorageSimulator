c8db0511a5f9d5ecf7f52437039ef9fc7dd2c91f
Toygoon <toygoon@gmail.com>
2022-01-19 13:42:50 +0900

    Rejuvenator wip

---
+++ src/algorithms/Rejuvenator.java
@@ -0,0 +1,16 @@
+/* Simulation of Rejuvenator Wear Leveling Algorithms
+ * Efficient Management for Large-Scale Flash-Memory Storage Systems with Resource Conservation, 401p
+ *
+ * @author Lim Jung Min,
+ * Department of Computer Engineering, Yeungnam University.
+ */
+
+package algorithms;
+
+import flash.FlashStorage;
+
+public class Rejuvenator {
+    public static void rejuvMain(FlashStorage flashStorage) {
+
+    }
+}
